680dea848ca1e64cc2dc79f7f0f111ec190fb149
Vlada Fursa <108814668+vladafursa@users.noreply.github.com>
2026-09-18 00:28:41 +0100

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        for(int i=0;i<=31;i++){
+            if((n&(1<<i))>0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
